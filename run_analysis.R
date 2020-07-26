3666517f40f02e5b8642c706cc3baeb0b8b88386
theSLWayne <hiranhasanka@gmail.com>
2020-07-27 01:36:42 +0530

    Added README.md

--- run_analysis.R
+++ run_analysis.R
@@ -4,3 +4,6 @@ library(markdown)
 # Creating analysis report
 knit("run_analysis.Rmd", output = 'run_analysis.md', encoding = 'UTF-8', quiet = T)
 markdownToHTML('run_analysis.md', 'run_analysis.html')
+
+# Writing data table with tidy data into a .txt file as requested
+write.table(dTableTidy, 'TidyData.txt', quote = F, sep = "\t", row.names = F)
